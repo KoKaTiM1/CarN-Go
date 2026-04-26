4f75970101368ac92a9c043bc370886c8296f77e
KoKaTiM1 <progamer0001001001@gmail.com>
2026-04-26 12:49:47 +0300

    Add Firebase utility class

---
+++ app/src/main/java/com/yardenbental_danielcohen_shlomoedelstein/carn_go/firebase/Firebase.java
@@ -0,0 +1,20 @@
+package com.yardenbental_danielcohen_shlomoedelstein.carn_go.firebase;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import android.util.Log;
+
+public class Firebase extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Handle incoming messages here
+        Log.d("FCM", "From: " + remoteMessage.getFrom());
+    }
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d("FCM", "Refreshed token: " + token);
+        // Upload token to your server if needed
+    }
+}
